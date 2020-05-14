f52985fd2176e29f120dfadcd59816a3ecd8bb4c
Lukasz Szczupak <szczupak.lukasz@o2.pl>
2020-05-14 11:54:01 +0200

    Budzet domowy

--- src/main/java/home/budget/utils/FindTransaction.java
+++ src/main/java/home/budget/utils/FindTransaction.java
@@ -3,6 +3,7 @@ package home.budget.utils;
 import home.budget.Transaction;
 import home.budget.TransactionDao;
 
+import java.util.Optional;
 import java.util.Scanner;
 
 public class FindTransaction {
@@ -12,8 +13,8 @@ public class FindTransaction {
         System.out.println("Wpisz rodzaj transakcji aby wyświetlić przychody lub wydatki:");
         String type = scanner.nextLine();
 
-        TransactionDao transactionDAO = new TransactionDao();
-        Transaction transaction = transactionDAO.findByType(type);
+        TransactionDao transactionDao = new TransactionDao();
+        Optional<Transaction> transaction = transactionDao.findByType(type);
         System.out.println(transaction);
     }
 }
